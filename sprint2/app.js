6b709861ac8e3dcaf70e34e49898acd19a66d0e9
ErnestoC10 <sanchezernestoalejandro@gmail.com>
2021-08-09 12:10:09 -0300

    crea estructura inicial

---
+++ sprint2/app.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express ();
+const port = 3030;
+const path = require ('path');
+
+app.use(express.static('public'))
+app.listen(port, () => console.log ('Server running in port' + port))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'detailProduct.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')));
