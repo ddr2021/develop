dfa997153a788e26a48c9ca38ce2460a9c93d284
ddr2021 <dannieldavvid2004@gmail.com>
2021-08-11 04:55:37 -0300

    Completando algunas congiguraciones. Renombrado de archivos atutentication... y cart...

--- sprint2/app.js
+++ sprint2/app.js
@@ -1,13 +1,19 @@
+//EXPRESS
 const express = require ('express');
 const app = express ();
+
+//SERVIDOR
 const port = 3030;
-const path = require ('path');
+app.listen(port, () => console.log (`Server receiving on port ${port}`))
 
+//RUTAS ESTATICAS
 app.use(express.static('public'))
-app.listen(port, () => console.log ('Server running in port' + port))
 
+//PETICIONES Y RESPUESTAS AL SERVIDOR
+const path = require ('path');
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'detailProduct.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')));
+app.get('/cart', (req, res) => res.sendFile(path.join(__dirname, 'views', 'cart.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/autentication', (req, res) => res.sendFile(path.join(__dirname, 'views', 'autentication.html')));
+
